a2ae8a8fd11c64f417ae5c937a46146d5461bd26
mdshantosharker <mdshantosharker@gmail.com>
2026-05-04 11:54:40 +0600

    added new tranding section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import ExtraSection from "@/components/ExtraSection";
 import Instructors from "@/components/Instructors";
 import Learning from "@/components/Learning";
 import PopularCourses from "@/components/PopularCourses";
+import TrandingCourses from "@/components/TrandingCourses";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="bg-slate-100">
         <div className="container mx-auto px-6 lg:px-10 pt-16 ">
           <PopularCourses />
+          <TrandingCourses/>
           <ExtraSection></ExtraSection>
         </div>
         <Learning />
---
+++ src/components/TrandingCourses.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TrandingCourses = () => {
+  const courses = [
+    "Full Stack Web Development",
+    "React & Next.js Mastery",
+    "Data Structures & Algorithms",
+    "UI/UX Design Fundamentals",
+    "Backend Development with Node.js",
+    "Python for Beginners",
+  ];
+
+  return (
+    <section className="py-16 text-slate-800">
+      <div className="">
+        <h2 className="text-6xl mb-3 font-bold text-center text-slate-900">
+          🔥Trending Courses
+        </h2>
+
+        <p className="text-center text-slate-500 mt-2 text-sm">
+          Most popular learning paths in SkillSphere
+        </p>
+
+        <div className="mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {courses.map((course, index) => (
+            <div
+              key={index}
+              className="bg-white border border-slate-200 hover:border-slate-300 
+          hover:shadow-lg transition p-6 rounded-xl cursor-pointer"
+            >
+              <h3 className="text-lg font-semibold text-slate-900">{course}</h3>
+
+              <p className="text-sm text-slate-500 mt-2">
+                Start learning and build real-world projects with hands-on
+                practice.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TrandingCourses;
